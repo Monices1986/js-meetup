5ff6fba03b57d49a626fcb7520bc0cb6b2956cc5
Monices1986 <36625423+Monices1986@users.noreply.github.com>
2023-02-28 15:33:14 +0100

    add gallery template

--- gallery/main.js
+++ gallery/main.js
@@ -1,1 +1,12 @@
-let images = [];
+let counter = 2;
+
+function changeImage(image) {
+    let imageNumberToShow = counter.toString().padStart(3, '0');
+    let nextImageUrl = `./img/landscape-${imageNumberToShow}-small.jpg`;
+    image.src = nextImageUrl;
+    counter = (counter + 1)
+}
+
+function click() {
+    console.log("someone clicked the button")
+  }
